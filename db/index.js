da8f238bdab5900f4a77dac195c05a2a700529dc
guozhao709 <guozhao709@qq.com>
2026-03-19 22:20:14 +0800

    C端静态页面制作完成

--- db/index.js
+++ db/index.js
@@ -1,5 +1,3 @@
 import Database from "better-sqlite3";
-
 const db = new Database("./db/village.db");
-
 export default db;
--- hooks/user.js
+++ hooks/user.js
@@ -1,4 +1,5 @@
 import userAuthRoutes from "../routes/client/userAuth.js";
+import {authMiddleware} from "../tools/userJwt.js"
 import userAIchatRoutes from "../routes/client/userAIchatWeb.js";
 
 const userRouter = (app) => {
@@ -6,7 +7,7 @@ const userRouter = (app) => {
   app.use("/user/auth", userAuthRoutes);
 
   // 用户AI聊天路由
-  app.use("/user/AIchat", userAIchatRoutes);
+  app.use("/user/AIchat", authMiddleware, userAIchatRoutes);
 };
 
 export default userRouter;
